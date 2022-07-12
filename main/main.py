b417fe28069900f2c8f6fecce96c098ef8746775
Pedro <joao.pedro73210@gmail.com>
2022-07-12 02:15:37 -0300

    tests with text object (canvas.beginText()) functions

--- main/main.py
+++ main/main.py
@@ -1,10 +1,11 @@
+from urllib.error import ContentTooShortError
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
 import PyPDF2 as ppdf
 import abnt
 
 cnv_teste = canvas.Canvas("pdf_teste/teste.pdf", pagesize=A4)
-
+#define title and name of the PDF what will be generate
 cnv_clone = canvas.Canvas("pdf_teste/testeclone.pdf", pagesize=A4)
 #open the pdf file
 file = open(r'C:\Users\joaop\Documents\PDF_auto\Arquivos Base\Diagnóstico_CTT.pdf','rb')
@@ -20,20 +21,39 @@ for i in range(file_content.numPages):
     pages.append(file_content.getPage(i))
 
 #extracting the content of the pages
-content = []
+contents = []
 
 for i in range(file_content.numPages):
-    content.append(pages[i].extractText())
+    contents.append(pages[i].extractText())
 
 # for i in range(100):
 #     cnv_clone.drawString(x,y,f"{i}")
 #     y += 20
     
-for i in range(file_content.numPages):
-    cnv_clone.drawString(0,750,content[i])
-    cnv_clone.showPage()
+# for i in range(file_content.numPages):
+#     cnv_clone.drawString(0,750,content[i])
+#     cnv_clone.showPage()
 
-print(abnt.a)
+def cursormoves1(canvas,contents):
+    from reportlab.lib.units import inch
+    textobject = canvas.beginText()
+    textobject.setTextOrigin(0, 800)
+    textobject.setFont("Helvetica-Oblique", 14)
+    print(type(contents[0]))
+    textobject.textLines(contents[0]) #escreve em textObject algumas linhas
+    # for content in contents:
+    #     textobject.textLine(content)
+    #     # textobject.showPage()
+    
+    textobject.setFillGray(0.4)
+    textobject.textLines('''
+        teste
+	''')
+    canvas.drawText(textobject) #desenha no objeto Canva o que está escrito no objeto do tipo canva.beginText()
+
+cursormoves1(cnv_clone,contents)
+
+# print(abnt.a)
 
 #saving the final PDF file created
 cnv_clone.save()
--- main/test.py
+++ main/test.py
@@ -2,3 +2,34 @@ from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
 import PyPDF2 as ppdf
 import abnt
+
+cnv_teste = canvas.Canvas("pdf_teste/teste.pdf", pagesize=A4)
+
+cnv_clone = canvas.Canvas("pdf_teste/testeclone.pdf", pagesize=A4)
+#open the pdf file
+file = open(r'C:\Users\joaop\Documents\PDF_auto\Arquivos Base\Diagnóstico_CTT.pdf','rb')
+
+#reading the pdf file content
+file_content = ppdf.PdfFileReader(file) #retorna um objeto criado pelo PDf presente no caminho passado no argumento
+
+#getting the page objects of PDF file
+
+def cursormoves1(canvas):
+    from reportlab.lib.units import inch
+    textobject = canvas.beginText()
+    textobject.setTextOrigin(inch, 2.5*inch)
+    textobject.setFont("Helvetica-Oblique", 14)
+    
+    lyrics = ["teste 1","teste 2","teste 3","teste 4","teste 5"]
+    for line in lyrics:
+        textobject.textLine(line)
+    
+    textobject.setFillGray(0.4)
+    textobject.textLines('''
+        ado ado ado quem ta lendo e arrombado ado ado ado 
+	''')
+    canvas.drawText(textobject)
+
+cursormoves1(cnv_clone)
+
+cnv_clone.save()
