08efa257a2046c6954c88d45f915e98dafa08a8b
Pedro <joao.pedro73210@gmail.com>
2022-07-15 21:18:29 -0300

    creating .gitignore

--- main/models.py
+++ main/models.py
@@ -1,3 +1,5 @@
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
 import PyPDF2 as ppdf
+import xlrd 
+import numpy as np
--- main/pdflib.py
+++ main/pdflib.py
@@ -4,6 +4,7 @@ from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
 import PyPDF2 as ppdf
 from PIL import Image
+import models as md
 
 def draw_text_pdf(path_to_new_pdf_file,contents):
     from reportlab.lib.units import inch
@@ -36,7 +37,21 @@ def extract_content(path):
 def mm2p(mm):
     return mm/0.352777
 
+def verify_list(list: array):
+    a = 0
+    for j in list['/Resources']:
+        if j == '/XObject':
+            a = 1
+    return a
+
 ##pass the PDF file path to extract PDF images 
+
+class image:
+    def __init__(self,type,name):
+        self.type = type
+        self.name = name
+
+
 
 def extract_image(file):
     if file:
@@ -47,6 +62,7 @@ def extract_image(file):
             image_pages.append(input.getPage(i))
         
         objects = []
+        
         images = []
 
         for i in range(input.numPages):
@@ -65,24 +81,21 @@ def extract_image(file):
                             img = Image.frombytes(mode,size,data)
                             img.save('/Users/joaop/Documents/PDF_auto/Arquivos Base/images/'+obj[1:]+'.png')
                             images.append(img)
+                            images.append(obj[1:])
                         elif objects[i][obj]['/Filter'] == '/DCTDecode':
                             img = open('/Users/joaop/Documents/PDF_auto/Arquivos Base/images/'+obj[1:]+'.jpg','wb')
                             img.write(data)
                             img.close()
                             images.append(img)
+                            images.append(obj[1:])
                         elif objects[i][obj]['/Filter'] == '/JPXDecode':
                             img = open('/Users/joaop/Documents/PDF_auto/Arquivos Base/images/'+obj[1:]+'jp2','wb')
                             img.write(data)
                             img.close()
                             images.append(img)
+                            images.append(obj[1:])
             else:
                 objects.append(None)
+        print(images)
     else: 
         print('erro')
-
-def verify_list(list: array):
-    a = 0
-    for j in list['/Resources']:
-        if j == '/XObject':
-            a = 1
-    return a
